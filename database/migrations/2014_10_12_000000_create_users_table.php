26530704ab79a458726de85a753602cbca33ee66
TagNinr_Nguyen <tagninr.nguyen@gmail.com>
2018-04-17 17:04:40 +0700

    Update migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('fullname');
-            $table->string('username');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->boolean('sex');
+            $table->string('phone');
+            $table->strind('address');
             $table->rememberToken();
             $table->timestamps();
         });
